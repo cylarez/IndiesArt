f8f4c9b0b09c7331071cd807c0d5a8b3c231e45b
Cylarez <cylarez@gmail.com>
2011-04-19 11:18:57 -0400

    Add conf file sample

---
+++ settings_local.sample.py
@@ -0,0 +1,24 @@
+# Site conf
+MAIN_DIR = ''
+BLOG_URL = ''
+DEBUG = True
+SITE_ID = 1
+
+# Database conf
+DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+
+# Email server conf
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
